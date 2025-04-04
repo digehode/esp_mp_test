5fdaf6147f9d0ce26f4495cd854478d7cedfc88b
James Shuttleworth <digehode@googlemail.com>
2025-04-05 00:07:45 +0100

    Moved to splitting app code out and having a bootstrap main.py

--- src/blink.py
+++ src/blink.py
@@ -1,10 +1,12 @@
 from machine import Pin
 from time import sleep
 
-led = Pin(8, Pin.OUT)
 
-while True:
-    led.value(1)
-    sleep(1)
-    led.value(0)
-    sleep(1)
+def main():
+    led = Pin(8, Pin.OUT)
+
+    while True:
+        led.value(1)
+        sleep(1)
+        led.value(0)
+        sleep(1)
